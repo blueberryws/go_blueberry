191d60c03b5cb303236a3f178e7977986869cc0e
Cody Nelson <codeacon@gmail.com>
2024-07-06 13:18:50 -0600

    Pages Initial

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go_blueberry.blueberrywebservices.com
+
+go 1.22.4
---
+++ pages/html_pages.go
@@ -0,0 +1,23 @@
+package pages 
+
+import (
+    "embed"
+    "html/template"
+    "path"
+)
+
+func initTemplates(templateFS embed.FS, templateBasePath string) *template.Template {
+    var pageTemplates *template.Template = template.New("pages")
+    if len(templateBasePath) == 0 {
+        templateBasePath = "."
+    }
+    templates, err := templateFS.ReadDir(templateBasePath)
+    if err != nil {
+        panic(err)
+    }
+    for _, templateFile := range templates {
+        templateFullPath := path.Join(templateBasePath, templateFile.Name())
+        template.Must(pageTemplates.ParseFS(templateFS, templateFullPath))
+    }
+    return pageTemplates
+}
